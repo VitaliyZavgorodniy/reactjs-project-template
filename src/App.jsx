e69f1de34d1605c4e2431b32c5e490ff410ae807
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-04-22 11:56:14 +0300

    refactor

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Section from 'components/UI/Section';
+import Title from 'components/Title';
+
+const App = () => {
+  return (
+    <Section>
+      <Title />
+    </Section>
+  );
+};
+
+export default App;
